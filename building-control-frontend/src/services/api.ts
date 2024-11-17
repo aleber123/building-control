b4ee2115564363d48627f2f1ab6b2c3e5d9c5a16
aleber123 <alexander.bergqvist@gmail.com>
2024-11-17 19:52:36 +0100

    updates

--- building-control-frontend/src/services/api.ts
+++ building-control-frontend/src/services/api.ts
@@ -1,25 +1,25 @@
 import axios from 'axios';
+import { Building } from '../types/types';
 
 const api = axios.create({
   baseURL: 'http://localhost:3001'  // Update this URL if your backend is hosted elsewhere
 });
 
-export const fetchBuildings = async () => {
-  const response = await api.get('/buildings');
+export const fetchBuildings = async (): Promise<Building[]> => {
+  const response = await api.get<Building[]>('/buildings');
   return response.data;
 };
 
-export const createBuilding = async (building: any) => {
-  const response = await api.post('/buildings', building);
+export const createBuilding = async (building: Omit<Building, 'id'>): Promise<Building> => {
+  const response = await api.post<Building>('/buildings', building);
   return response.data;
 };
 
-export const updateBuilding = async (id: number, building: any) => {
-  const response = await api.put(`/buildings/${id}`, building);
+export const updateBuilding = async (id: number, building: Partial<Building>): Promise<Building> => {
+  const response = await api.put<Building>(`/buildings/${id}`, building);
   return response.data;
 };
 
-export const deleteBuilding = async (id: number) => {
-  const response = await api.delete(`/buildings/${id}`);
-  return response.data;
+export const deleteBuilding = async (id: number): Promise<void> => {
+  await api.delete(`/buildings/${id}`);
 };
